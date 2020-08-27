95436b03a9937a74473c0603a95c3600e0cdc6ad
rishitsaiya <rishitsaiya@gmail.com>
2020-08-27 10:09:16 +0530

    Added sms - crypto

---
+++ crypto/sms/sms.py
@@ -0,0 +1,3 @@
+r="smmmmsmssmmmmssmsmmsssmmsmmssmsssmmmssmmsmmmsmmmsmmmsssmssmsmmmssmmsmmsmsmmsmmsmssmsmmmmssmmsssmssmmsmsssmmsmmsmsmsssmmssmsmsmmmsmsmmsmssmssssmmsmsmmsms"
+r_new = r.replace('s','0').replace('m','1')
+print("Message: " + r_new)
